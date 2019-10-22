b32474fe67b00d2b47f8cdac3d00ea8c06e6b11f
Kushal Gaur <kusgaur@publicisgroupe.net>
2019-10-22 12:33:53 +0530

    updated products page with images

--- src/components/List.js
+++ src/components/List.js
@@ -11,11 +11,14 @@ class List extends Component {
         {products.map((item) => {
           return (
             <article key={item.key}>
-              <img src={`.`+item.imageUrl} alt=""></img>
+              <img src={`.`+item.imageURL} alt=""></img>
               <div className="text">
                 <h3>{item.name}</h3>
                 <p>{item.description}</p>
-                <button>Here's why</button>
+                <div>
+                <span>MRP Rs.{item.price}</span>
+                <button>Buy Now</button>
+                </div>
               </div>
             </article>
           )
