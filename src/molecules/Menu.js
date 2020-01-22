75f5b1cc3e1bc18197c20937ee168df4d8b71866
Kushal Gaur <kusgaur@publicisgroupe.net>
2020-01-22 10:36:30 +0530

    updated menu moile

--- src/molecules/Menu.js
+++ src/molecules/Menu.js
@@ -36,13 +36,13 @@ class Menu extends Component {
         return (
             <aside className="app__main-nav">
                 <ul>
-                {categories.map((item, index) => {
-                    return (
-                        <li key={index}>
-                            <Link to="/products" role="navigation" title={item.name} className="app_main-nav-links" onClick={() => this.props.simplifiedFunction(item.id)}>{item.name}</Link>
-                        </li>
-                    )
-                })}
+                    {categories.map((item, index) => {
+                        return (
+                            <li key={index}>
+                                <Link to="/products" role="navigation" title={item.name} className="app_main-nav-links" onClick={() => this.props.simplifiedFunction(item.id)}>{item.name}</Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </aside>
         );
@@ -76,38 +76,54 @@ class MobileMenu extends Component {
             })
     }
 
+    setMobileProductList = (event) => {
+        const val = event.target.value;
+        this.props.simplifiedFunction(val);
+    }
+
     render() {
         const { categories } = this.state;
         const firstCat = categories.length > 0 ? categories[0] : {};
         const slicedCats = categories.slice(1, categories.length);
+        const defaultVal = {
+            name: "Select",
+            value: "Select",
+            id: "0"
+        }
 
         return (
-            <header role="complementary" className="app__main-head">
-                <div id="app__menu-mobile">
-                    <div className="app__menu-mobile-brand">
-                        <Link to="/products" role="navigation" className="items" title={firstCat} onClick={() => this.props.simplifiedFunction(firstCat.id)}>{firstCat.name}</Link>
-                        <i className="fa fa-angle-down icon" data-toggle="collapse" data-target="#navbarCollapse"> </i>
-                    </div>
+            // <div className="app__main-head">
+            //     <div id="app__menu-mobile">
+            //         <div className="app__menu-mobile-brand">
+            //             <Link to="/products" role="navigation" className="items" title={firstCat} onClick={() => this.props.simplifiedFunction(firstCat.id)}>{firstCat.name}</Link>
+            //             <i className="fa fa-angle-down icon" data-toggle="collapse" data-target="#navbarCollapse"> </i>
+            //         </div>
 
-                    <div className="collapse navbar-collapse" id="navbarCollapse">
-                        <ul className="navbar-nav">
-                            {slicedCats.map((item) => {
-                                return (
-                                    <li key={item.key}>
-                                        <Link to="/products" role="navigation" title={item.name} className="app__main-nav-links" onClick={() => this.props.simplifiedFunction(item.id)}>
-                                            {item.name}</Link>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
-                    {/* <select>
-                    <option value="Orange">Orange</option>
-                    <option value="Radish">Radish</option>
-                    <option value="Cherry">Cherry</option>
-                    </select> */}
-                </div>
-            </header>
+            //         <div className="collapse navbar-collapse" id="navbarCollapse">
+            //             <ul className="navbar-nav">
+            //                 {slicedCats.map((item) => {
+            //                     return (
+            //                         <li key={item.key}>
+            //                             <Link to="/products" role="navigation" title={item.name} className="app__main-nav-links" onClick={() => this.props.simplifiedFunction(item.id)}>
+            //                                 {item.name}</Link>
+            //                         </li>
+            //                     )
+            //                 })}
+            //             </ul>
+            //         </div>
+            //     </div>
+
+            // </div>
+            <select className="app__mobile-menu" onChange={this.setMobileProductList.bind(this)}>
+                <option value="default" selected>Select Option</option>
+                {categories.map((item) => {
+                    return (
+                        <option key={item.key} className="app__main-nav-links" value={item.id}>
+                            {item.name}
+                        </option>
+                    )
+                })}
+            </select>
         );
     }
 }
